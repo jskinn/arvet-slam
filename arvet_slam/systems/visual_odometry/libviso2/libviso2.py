7d57192541ebd184962b57e8b52ca6cea8fef6f5
John Skinner <jskinn@protonmail.com>
2019-06-13 23:51:02 +1000

    Update libviso with changes to the API

--- arvet_slam/systems/visual_odometry/libviso2/libviso2.py
+++ arvet_slam/systems/visual_odometry/libviso2/libviso2.py
@@ -6,14 +6,16 @@ import pymodm.fields as fields
 from .viso2 import Stereo_parameters, VisualOdometryStereo
 
 import arvet.util.image_utils as image_utils
-import arvet.core.sequence_type
-import arvet.core.system
-import arvet.core.trial_result
+from arvet.metadata.camera_intrinsics import CameraIntrinsics
+from arvet.core.image import Image
+from arvet.core.image_source import ImageSource
+from arvet.core.sequence_type import ImageSequenceType
+from arvet.core.system import VisionSystem
 import arvet.util.transform as tf
 from arvet_slam.trials.slam.visual_slam import FrameResult, SLAMTrialResult
 
 
-class LibVisOSystem(arvet.core.system.VisionSystem):
+class LibVisOSystem(VisionSystem):
     """
     Class to run LibVisO2 as a vision system.
     """
@@ -38,7 +40,7 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         """
 
         """
-        super().__init__(*args, **kwargs)
+        super(LibVisOSystem, self).__init__(*args, **kwargs)
 
         # These will get overridden by set_camera_intrinisics
         self._focal_distance = 1.0
@@ -52,14 +54,14 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         self._estimated_world_pose = None
         self._frame_results = []
 
-    def is_image_source_appropriate(self, image_source):
-        return (image_source.sequence_type == arvet.core.sequence_type.ImageSequenceType.SEQUENTIAL and
-                image_source.is_stereo_available)
-
-    def is_deterministic(self):
+    def is_deterministic(self) -> bool:
         return True
 
-    def set_camera_intrinsics(self, camera_intrinsics):
+    def is_image_source_appropriate(self, image_source: ImageSource) -> bool:
+        return (image_source.sequence_type == ImageSequenceType.SEQUENTIAL and
+                image_source.is_stereo_available)
+
+    def set_camera_intrinsics(self, camera_intrinsics: CameraIntrinsics) -> None:
         """
         Set the camera intrinisics for libviso2
         :param camera_intrinsics: The camera intrinsics, relative to the image resolution
@@ -70,19 +72,20 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         self._cu = float(camera_intrinsics.cx)
         self._cv = float(camera_intrinsics.cy)
 
-    def set_stereo_baseline(self, baseline):
+    def set_stereo_offset(self, offset: tf.Transform) -> None:
         """
         Set the stereo baseline
-        :param baseline:
+        :param offset:
         :return:
         """
+        baseline = -1 * offset.location[1]   # right is -Y axis
         logging.getLogger(__name__).error("Setting stereo baseline to {0}".format(baseline))
         self._base = float(baseline)
 
-    def start_trial(self, sequence_type):
+    def start_trial(self, sequence_type: ImageSequenceType) -> None:
         logging.getLogger(__name__).error("Starting LibVisO trial...")
-        if not sequence_type == arvet.core.sequence_type.ImageSequenceType.SEQUENTIAL:
-            return False
+        if not sequence_type == ImageSequenceType.SEQUENTIAL:
+            return
         params = Stereo_parameters()
         logging.getLogger(__name__).error("    Created parameters object, populating ...")
 
@@ -124,7 +127,7 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         self._frame_results = []
         logging.getLogger(__name__).error("    Started LibVisO trial.")
 
-    def process_image(self, image, timestamp):
+    def process_image(self, image: Image, timestamp: float) -> None:
         logging.getLogger(__name__).error("Processing image at time {0} ...".format(timestamp))
         left_grey = prepare_image(image.left_pixels)
         right_grey = prepare_image(image.right_pixels)
@@ -137,7 +140,7 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         np_motion = np.zeros((4, 4))
         motion.toNumpy(np_motion)
         np_motion = np.linalg.inv(np_motion)    # Invert the motion to make it new frame relative to old
-        relative_pose = make_relative_pose(np_motion) # This is the pose of the previous pose relative to the next one
+        relative_pose = make_relative_pose(np_motion)  # This is the pose of the previous pose relative to the next one
         self._estimated_world_pose = self._estimated_world_pose.find_independent(relative_pose)
         logging.getLogger(__name__).error("    got estimated motion ...")
 
@@ -155,7 +158,7 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         ))
         logging.getLogger(__name__).error("    Processing done.")
 
-    def finish_trial(self):
+    def finish_trial(self) -> SLAMTrialResult:
         logging.getLogger(__name__).error("Finishing LibVisO trial ...")
         result = SLAMTrialResult(
             system=self,
@@ -171,6 +174,88 @@ class LibVisOSystem(arvet.core.system.VisionSystem):
         logging.getLogger(__name__).error("    Created result")
         return result
 
+    @classmethod
+    def preload_image_data(cls, image: Image) -> None:
+        """
+        Preload the pixel data we use from the images.
+        This is a stereo system, load right pixel data as well
+        :param image:
+        :return:
+        """
+        super(LibVisOSystem, cls).preload_image_data(image)
+        if hasattr(image, 'right_pixels'):
+            _ = image.right_pixels
+
+    @classmethod
+    def get_instance(
+            cls,
+            matcher_nms_n=3,
+            matcher_nms_tau=50,
+            matcher_match_binsize=50,
+            matcher_match_radius=200,
+            matcher_match_disp_tolerance=2,
+            matcher_outlier_disp_tolerance=5,
+            matcher_outlier_flow_tolerance=5,
+            matcher_multi_stage=True,
+            matcher_half_resolution=True,
+            matcher_refinement=50,
+            bucketing_max_features=2,
+            bucketing_bucket_width=50,
+            bucketing_bucket_height=50,
+            ransac_iters=200,
+            inlier_threshold=2.0,
+            reweighting=True
+    ) -> 'LibVisOSystem':
+        """
+        Get an instance of this vision system, with some parameters, pulling from the database if possible,
+        or construct a new one if needed.
+        It is the responsibility of subclasses to ensure that as few instances of each system as possible exist
+        within the database.
+        Does not save the returned object, you'll usually want to do that straight away.
+        :return:
+        """
+        # Look for existing objects with the same settings
+        all_objects = LibVisOSystem.objects.raw({
+            'matcher_nms_n': matcher_nms_n,
+            'matcher_nms_tau': matcher_nms_tau,
+            'matcher_match_binsize': matcher_match_binsize,
+            'matcher_match_radius': matcher_match_radius,
+            'matcher_match_disp_tolerance': matcher_match_disp_tolerance,
+            'matcher_outlier_disp_tolerance': matcher_outlier_disp_tolerance,
+            'matcher_outlier_flow_tolerance': matcher_outlier_flow_tolerance,
+            'matcher_multi_stage': matcher_multi_stage,
+            'matcher_half_resolution': matcher_half_resolution,
+            'matcher_refinement': matcher_refinement,
+            'bucketing_max_features': bucketing_max_features,
+            'bucketing_bucket_width': bucketing_bucket_width,
+            'bucketing_bucket_height': bucketing_bucket_height,
+            'ransac_iters': ransac_iters,
+            'inlier_threshold': inlier_threshold,
+            'reweighting': reweighting
+        })
+        if all_objects.count() > 0:
+            return all_objects.first()
+        # There isn't an existing system with those settings, make a new one.
+        obj = cls(
+            matcher_nms_n=matcher_nms_n,
+            matcher_nms_tau=matcher_nms_tau,
+            matcher_match_binsize=matcher_match_binsize,
+            matcher_match_radius=matcher_match_radius,
+            matcher_match_disp_tolerance=matcher_match_disp_tolerance,
+            matcher_outlier_disp_tolerance=matcher_outlier_disp_tolerance,
+            matcher_outlier_flow_tolerance=matcher_outlier_flow_tolerance,
+            matcher_multi_stage=matcher_multi_stage,
+            matcher_half_resolution=matcher_half_resolution,
+            matcher_refinement=matcher_refinement,
+            bucketing_max_features=bucketing_max_features,
+            bucketing_bucket_width=bucketing_bucket_width,
+            bucketing_bucket_height=bucketing_bucket_height,
+            ransac_iters=ransac_iters,
+            inlier_threshold=inlier_threshold,
+            reweighting=reweighting
+        )
+        return obj
+
 
 def make_relative_pose(frame_delta):
     """
@@ -185,9 +270,9 @@ def make_relative_pose(frame_delta):
     """
     frame_delta = np.asarray(frame_delta)
     coordinate_exchange = np.array([[0, 0, 1, 0],
-                                     [-1, 0, 0, 0],
-                                     [0, -1, 0, 0],
-                                     [0, 0, 0, 1]])
+                                    [-1, 0, 0, 0],
+                                    [0, -1, 0, 0],
+                                    [0, 0, 0, 1]])
     pose = np.dot(np.dot(coordinate_exchange, frame_delta), coordinate_exchange.T)
     return tf.Transform(pose)
 
